4680ecf7e6aee5aa2167e0727bdffeda8f4dcc36
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-03-31 18:43:59 +0200

    Adds UserDAO with save, findById and findByEmail methods

---
+++ src/main/java/AntoninoPalazzolo/dao/UserDAO.java
@@ -0,0 +1,39 @@
+package AntoninoPalazzolo.dao;
+
+import AntoninoPalazzolo.entities.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+
+import java.util.UUID;
+
+public class UserDAO {
+    private EntityManager em;
+
+    public UserDAO(EntityManager em){
+        this.em = em;
+    }
+
+    public void save(User user){
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(user);
+        transaction.commit();
+    }
+
+    public User findById(UUID id){
+        return em.find(User.class, id);
+    }
+
+    public User findByEmail(String email){
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.userEmail = :email", User.class);
+        query.setParameter("email", email);
+
+        try{
+            return query.getSingleResult();
+        }catch (NoResultException e){
+            return null;
+        }
+    }
+}

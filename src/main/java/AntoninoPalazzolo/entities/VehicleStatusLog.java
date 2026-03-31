dc967e2971c70da887390e7d747db424ee9c77e2
palax-99 <ninociao7@gmail.com>
2026-03-31 08:26:43 +0200

    Creata la classe VehicleStatusLog per tracciare lo storico degli stati dei veicoli
    Ogni cambio di stato genera un nuovo
    record invece di modificare quello esistente, garantendo uno storico completo.
    La data di aggiornamento viene impostata automaticamente tramite LocalDateTime.now().

---
+++ src/main/java/AntoninoPalazzolo/entities/VehicleStatusLog.java
@@ -0,0 +1,64 @@
+package AntoninoPalazzolo.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "vehicle_status_log")
+public class VehicleStatusLog {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_vehicle_status_log")
+    private UUID idVehicleStatusLog;
+
+    @ManyToOne
+    @JoinColumn(name = "id_vehicle", nullable = false)
+    private Vehicle vehicle;
+
+    @Column(name = "vehicle_availability_updated_on", nullable = false)
+    private LocalDateTime vehicleAvailabilityUpdatedOn;
+
+    @Column(name = "vehicle_in_service", nullable = false)
+    private boolean vehicleInService;
+
+    @Column(name = "permanently_out_of_service", nullable = false)
+    private boolean permanentlyOutOfService;
+
+    protected VehicleStatusLog() {
+    }
+
+    public VehicleStatusLog(Vehicle vehicle, boolean vehicleInService, boolean permanentlyOutOfService) {
+        this.vehicle = vehicle;
+        this.vehicleInService = vehicleInService;
+        this.permanentlyOutOfService = permanentlyOutOfService;
+        this.vehicleAvailabilityUpdatedOn = LocalDateTime.now();
+        //La data di aggiornamento viene impostata automaticamente al momento
+        // della creazione del log
+    }
+
+    public UUID getIdVehicleStatusLog() {
+        return idVehicleStatusLog;
+    }
+
+    public LocalDateTime getVehicleAvailabilityUpdatedOn() {
+        return vehicleAvailabilityUpdatedOn;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleStatusLog{" +
+                "idVehicleStatusLog=" + idVehicleStatusLog +
+                ", vehicle=" + vehicle +
+                ", vehicleAvailabilityUpdatedOn=" + vehicleAvailabilityUpdatedOn +
+                ", vehicleInService=" + vehicleInService +
+                ", permanentlyOutOfService=" + permanentlyOutOfService +
+                '}';
+    }
+}

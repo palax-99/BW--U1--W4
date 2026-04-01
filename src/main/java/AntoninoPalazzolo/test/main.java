b400b2f84cbb017b7af52d49552d63fb40755afe
palax-99 <ninociao7@gmail.com>
2026-04-01 10:51:25 +0200

    create main.java
    creata la classe main, per testare i metodi

---
+++ src/main/java/AntoninoPalazzolo/test/main.java
@@ -0,0 +1,12 @@
+package AntoninoPalazzolo.test;
+
+import AntoninoPalazzolo.entities.Vehicle;
+import AntoninoPalazzolo.entities.VehicleType;
+
+public class main {
+    public static void main(String[] args) {
+        Vehicle veicolo1 = new Vehicle("AB123ER", VehicleType.BUS);
+        
+
+    }
+}
